8aedea7dd8ad5bb0bc1147beefdd2eaa2af98317
Đặng Tiến Đạt K15_HL <97070952+tiendat91@users.noreply.github.com>
2023-03-21 18:02:45 +0700

    update logo

--- NokNok_Shopping/NokNok/Pages/Index.cshtml.cs
+++ NokNok_Shopping/NokNok/Pages/Index.cshtml.cs
@@ -15,6 +15,8 @@ namespace NokNok.Pages
         [BindProperty]
         public List<Product> Products { get; set; }
         [BindProperty]
+        public List<Product> PopularProducts { get; set; }
+        [BindProperty]
         public List<Category> Categories { get; set; }
 
         public IndexModel()
@@ -28,14 +30,20 @@ namespace NokNok.Pages
 
         public async Task OnGet()
         {
-            HttpResponseMessage response = await client.GetAsync(ProductApiUrl);
-            string strData = await response.Content.ReadAsStringAsync();
             var options = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
             };
+
+            //PRODUCT
+            HttpResponseMessage response = await client.GetAsync(ProductApiUrl);
+            string strData = await response.Content.ReadAsStringAsync();
             Products = JsonSerializer.Deserialize<List<Product>>(strData, options);
 
+            //POPULAR PRODUCT
+            PopularProducts = Products.AsQueryable().OrderByDescending(s=>s.UnitPrice).Take(5).ToList();
+
+            //CATEGORY
             HttpResponseMessage responseC = await client.GetAsync(CategoryApiUrl);
             string strDataC = await responseC.Content.ReadAsStringAsync();
             Categories = JsonSerializer.Deserialize<List<Category>>(strDataC, options);
